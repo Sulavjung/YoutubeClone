c1acbe2d830179f80d62cf55da5656a6eaf07616
Sulavjung <79497159+Sulavjung@users.noreply.github.com>
2023-07-17 11:01:38 -0700

    Latest Changes to the Milestone 2

--- application/routes/index.js
+++ application/routes/index.js
@@ -3,7 +3,21 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'CSC 317 App', name:"Sulav Jung Hamal" });
+  res.render('index', { title: 'CSC 317 App', name:"Sulav Jung Hamal", js:["photoDisplay.js"]  });
+});
+
+/* GET Login Page.  */
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Login', name:"Sulav Jung Hamal"});
+});
+router.get('/postvideo', function(req, res, next) {
+  res.render('postvideo', { title: 'Post Video', name:"Sulav Jung Hamal" });
+});
+router.get('/registration', function(req, res, next) {
+  res.render('registration', { title: 'Registration', name:"Sulav Jung Hamal", js:["script.js"] });
+});
+router.get('/viewpost', function(req, res, next) {
+  res.render('viewpost', { title: 'View Post', name:"Sulav Jung Hamal",  });
 });
 
 module.exports = router;
